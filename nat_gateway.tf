d44518ea8879d12f91e6d09984fec78cec249ecd
marc <marcmodin@yahoo.com>
2021-07-11 16:58:00 +0200

    add ngw count by az

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_eip" "ngw" {
-  for_each = var.enable_nat_gateway == true ? local.public_subnets : {}
+  for_each = var.enable_nat_gateway == true ? local.nat_subnets : {}
   vpc      = true
 
   tags = { "Name" = format("%s-eip-%s", var.name, each.key) }
@@ -11,29 +11,28 @@ resource "aws_eip" "ngw" {
 }
 
 resource "aws_nat_gateway" "ngw" {
-  for_each      = var.enable_nat_gateway == true ? local.public_subnets : {}
+  for_each      = var.enable_nat_gateway == true ? local.nat_subnets : {}
   allocation_id = aws_eip.ngw[each.key].id
   subnet_id     = aws_subnet.subnets[each.key].id
 
   tags = { "Name" = format("%s-ngw-%s", var.name, each.key) }
-
 }
 
 resource "aws_route_table" "ngw" {
-  for_each = var.enable_nat_gateway == true ? local.public_subnets : {}
+  for_each = var.enable_nat_gateway == true ? local.nat_subnets : {}
   vpc_id   = aws_vpc.main.id
-  tags     = { "Name" = format("%s-rt-%s", var.name, each.key) }
+  tags     = { "Name" = format("%s-ngw-rt-%s", var.name, index(keys(local.nat_subnets), each.key)) }
 }
 
 resource "aws_route" "ngw" {
-  for_each               = var.enable_nat_gateway == true ? local.public_subnets : {}
+  for_each               = var.enable_nat_gateway == true ? local.nat_subnets : {}
   route_table_id         = aws_route_table.ngw[each.key].id
   nat_gateway_id         = aws_nat_gateway.ngw[each.key].id
   destination_cidr_block = "0.0.0.0/0"
 }
 
 resource "aws_route_table_association" "ngw" {
-  for_each       = var.enable_nat_gateway == true ? { for v in local.merged_ngw_subnets_map : v.subnet => v } : {}
+  for_each       = var.enable_nat_gateway == true ? { for v in local.merged_chunk_map : v.subnet => v } : {}
   route_table_id = aws_route_table.ngw[each.value.key].id
   subnet_id      = aws_subnet.subnets[each.value.subnet].id
 
@@ -44,5 +43,4 @@ resource "aws_route_table_association" "ngw" {
   depends_on = [
     aws_route_table.ngw
   ]
-
 }
