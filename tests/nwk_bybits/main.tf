86c841a5fca9c06ea1930df02390588a38b7934f
SebRosander <32144528+SebRosander@users.noreply.github.com>
2023-03-06 16:26:23 +0100

    fix(tests): adding tests (#16)
    improving internal testing of module to comply with new standards

---
+++ tests/nwk_bybits/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+module "nwk" {
+  source   = "../../"
+  name     = var.name
+  vpc_cidr = var.vpc_cidr
+  subnets_bybits = [
+    { name = "App", bits = 1, net = 1 }, { name = "Front", bits = 2, net = 1 }, { name = "DB", bits = 3, net = 1 },
+    { name = "Admin", bits = 3, net = 0 }
+  ]
+  availability_zone = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+}
+
+output "vpc_id" {
+  value = module.nwk.vpc_id
+}
--- tests/nwk_ha/variables.tf
+++ tests/nwk_ha/variables.tf
@@ -3,5 +3,6 @@ variable "vpc_cidr" {}
 variable "subnets_byname" {
   type = list(string)
 }
-variable "public_subnets" {}
-variable "key_pair_name" {}
+variable "public_subnets" {
+  type = list(string)
+}
